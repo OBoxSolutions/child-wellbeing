667eb5d50aff60a46c77496af76bcdc3e42a9859
Elodin <jcassola@uclv.cu>
2021-11-11 22:46:18 -0500

    fix typos in wp-config

--- wp-config.php
+++ wp-config.php
@@ -22,12 +22,12 @@
 /** The name of the database for WordPress */
 if(isset($_ENV[`MYSQL_DATABASE_URL`])) {
     $db = parse_url($_ENV[`MYSQL_DATABASE_URL`]);
-    define(‘DB_NAME’, trim($db[`path`],`/`));
-    define(‘DB_USER’, $db[`user`]);
-    define(‘DB_PASSWORD’, $db[`pass`]);
-    define(‘DB_HOST’, $db[`host`]);
-    define(‘DB_CHARSET’, ‘utf8’);
-    define(‘DB_COLLATE’, ‘’);
+    define('DB_NAME', trim($db[`path`],`/`));
+    define('DB_USER', $db[`user`]);
+    define('DB_PASSWORD', $db[`pass`]);
+    define('DB_HOST', $db[`host`]);
+    define('DB_CHARSET', 'utf8');
+    define('DB_COLLATE', '');
 } else {
     die('No Database credentials!');
 }
